ebdcaeaa9edcc84bf9f4396b8c1de4092dabf5c0
Nishit R Acharya <nishit@Nishits-MacBook-Air.local>
2025-06-07 18:46:42 +0530

    api handels 204 400 500

--- backend/src/router/getworkorder.js
+++ backend/src/router/getworkorder.js
@@ -3,20 +3,38 @@ const workOrderData = require('../database/db')
 
 router.post('/getworkorder', (req,res)=>{
     console.log('Get Work Order Router !');
-    const reqBody = req.body;
-    console.log(`req Body : ${JSON.stringify(reqBody)}`);
+    
+    try{
+        const reqBody = req.body;
+        console.log(`req Body : ${JSON.stringify(reqBody)}`);
 
-     // Filter based on only keys present in reqBody
-    const result = workOrderData.filter((data) => {
-        return Object.entries(reqBody).every(([key, value]) => {
-            console.log(data[key] , value)
-        if (!value) return true; // Skip empty/null/undefined filters
-        return data[key] && data[key].toString().toLowerCase() === value.toString().toLowerCase();
-        });
-    });
+        if(Object.keys(reqBody).length === 0){
+            console.log('No input provided !')
+            return res.status(404).send('Bad request');
+        }
 
-    console.log(`Result : ${JSON.stringify(result)}`)
-    res.status(200).json(result);
+        // Filter based on only keys present in reqBody
+       const result = workOrderData.filter((data) => {
+           return Object.entries(reqBody).every(([key, value]) => {
+               console.log(data[key] , value)
+           if (!value) return true; // Skip empty/null/undefined filters
+           return data[key] && data[key].toString().toLowerCase() === value.toString().toLowerCase();
+           });
+       });
+       
+       if(result){
+           console.log(`Result : ${JSON.stringify(result)}`)
+           res.status(200).json(result);
+       }else{
+            console.log('No records found !')
+            return res.status(204).send();
+       }
+
+    }catch(error){
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error')
+    }
+
 })
 
 module.exports = router;
